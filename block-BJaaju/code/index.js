4599977c394c9a4c9c9b6622c18f602eb808a9e4
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2023-08-11 18:22:11 -0400

    Refractor - Image Search API using Promises

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -19,6 +19,7 @@ Description:
 
 */
 
+/*
 const imagesList = document.querySelectorAll('.images img');
 const input = document.querySelector('input');
 const clientId = `4EdY7eMr6Y7YHuja80kX5BzBmuSl6Hlv4YYjOHfhB1k`;
@@ -26,8 +27,6 @@ input.addEventListener('keyup', handleChange);
 
 function displayUI(data) {
   let imageUrls = data.results.map((eachObj) => eachObj.urls.regular);
-  //   console.log(imageUrls);
-  //   console.log(imagesList);
   imagesList.forEach((img, i) => {
     img.src = imageUrls[i];
   });
@@ -45,11 +44,65 @@ function handleChange(event) {
       console.log('Data loading Started....');
       let imagesData = JSON.parse(xhr.response);
       displayUI(imagesData);
-      //   console.log(imagesData.alt_description);
-      //   console.log(imagesData.alt_description);
-      //   console.log(imagesData.urls.full);
       console.log(imagesData);
     };
     xhr.send();
+  }
+}
+
+*/
+
+/*
+.
+.
+.
+.
+.
+.
+.
+.*/
+
+// Refractor same image search app using PROMISES :-
+
+const imagesList = document.querySelectorAll('.images img');
+const input = document.querySelector('input');
+const clientId = `4EdY7eMr6Y7YHuja80kX5BzBmuSl6Hlv4YYjOHfhB1k`;
+input.addEventListener('keyup', handleChange);
+
+const getSearchURL = (query) =>
+  `https://api.unsplash.com/search/photos?client_id=4EdY7eMr6Y7YHuja80kX5BzBmuSl6Hlv4YYjOHfhB1k&page=1&per_page=15&query=${query}`;
+
+function fetch(url) {
+  return new Promise((res, rej) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => res(JSON.parse(xhr.response));
+    xhr.onerror = () => rej('Promise Rejected !!');
+    xhr.send();
+  });
+}
+
+// fetch(url)
+//   .then(displayUI)
+//   .catch((error) => console.log(error));
+
+function displayUI(data) {
+  let imageUrls = data.results.map((eachObj) => eachObj.urls.regular);
+  imagesList.forEach((img, i) => {
+    img.src = imageUrls[i];
+  });
+}
+
+function handleChange(event) {
+  if (event.keyCode == 13 && event.target.value !== '') {
+    fetch(getSearchURL(event.target.value))
+      .then((xhrResponseData) => displayUI(xhrResponseData))
+      .catch((error) => console.log(error));
+    // console.log(fetch(getSearchURL(event.target.value))); ->  This gives a Promise with [[PromiseResult]] as the xhr.response object.
+    /* Then using this xhrResponse Object, which is sent using then() to displatUI,
+     the xhr.response has a property called results which is an array. That is accessed to get images  
+      in the displayUI function. 
+    */
+    input.value = '';
   }
 }
